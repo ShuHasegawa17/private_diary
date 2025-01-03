241ad3c9ac41d19c4b1f473d6c0a751dc22ff0d2
shuhasegawa <shuhasegawa0430@gmail.com>
2025-01-03 12:35:03 +0900

    詳細機能の追加

--- diary/urls.py
+++ diary/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("inquiry/", views.InquiryView.as_view(), name="inquiry"),
     path("diary_list/", views.DiaryListView.as_view(), name="diary_list"),
+    path(
+        "diary-detail/<int:id>/", views.DiaryDetailView.as_view(), name="diary_detail"
+    ),
 ]
